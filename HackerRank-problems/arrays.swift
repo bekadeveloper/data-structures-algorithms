2f8c102a7ab41ef17678f4d1bd49acb3b9e1172d
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-10 18:00:00 +0500

    sales by match problem

--- HackerRank-problems/arrays.swift
+++ HackerRank-problems/arrays.swift
@@ -31,3 +31,24 @@ func matchingStrings(strings: [String], queries: [String]) -> [Int] {
 }
 
 matchingStrings(strings: ["aba","baba","aba","xzxb"], queries: ["aba","xzxb","ab"]) // returns [2, 1, 0]
+
+// Sales by Match (Easy)
+func sockMerchant(n: Int, ar: [Int]) -> Int {
+    var array = ar.sorted()
+    var count = 0
+    var i = 0
+    
+    while i < n-1 {
+        if array[i] == array[i+1] {
+            count += 1
+            i += 2
+        } else {
+            i += 1
+        }
+    }
+    
+    return count
+}
+
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+sockMerchant(n: 9, ar: ar) // returns 3
