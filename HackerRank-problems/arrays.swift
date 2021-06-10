ac61cdbe69489583313c5fcfa1caaeae9649605f
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-10 18:09:19 +0500

    compare triplets problem

--- HackerRank-problems/arrays.swift
+++ HackerRank-problems/arrays.swift
@@ -52,3 +52,21 @@ func sockMerchant(n: Int, ar: [Int]) -> Int {
 
 ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 sockMerchant(n: 9, ar: ar) // returns 3
+
+// Compare the Triplets (Easy)
+func compareTriplets(a: [Int], b: [Int]) -> [Int] {
+    var result: [Int] = [0, 0]
+    var i = 0
+    
+    while i < 3 {
+        if a[i] > b[i] {
+            result[0] += 1
+        } else if b[i] > a[i] {
+            result[1] += 1
+        }
+        i += 1
+    }
+    return result
+}
+
+compareTriplets(a: [17, 28, 30], b: [99, 16, 8]) // returns [2, 1]
