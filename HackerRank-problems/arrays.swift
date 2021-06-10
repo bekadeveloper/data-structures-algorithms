2ea384cf056e2620d2f2e06ced33bebb5ace470a
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-10 18:26:45 +0500

    diagonal difference

--- HackerRank-problems/arrays.swift
+++ HackerRank-problems/arrays.swift
@@ -70,3 +70,26 @@ func compareTriplets(a: [Int], b: [Int]) -> [Int] {
 }
 
 compareTriplets(a: [17, 28, 30], b: [99, 16, 8]) // returns [2, 1]
+
+// Diagonal Difference (Easy)
+func diagonalDifference(arr: [[Int]]) -> UInt {
+    var leftToRight: Int = 0
+    var rightToLeft: Int = 0
+    
+    var i = 0
+    var j = arr.count - 1
+    while i < arr.count {
+        leftToRight += arr[i][i]
+        rightToLeft += arr[i][j]
+        i += 1
+        j -= 1
+    }
+    return (leftToRight - rightToLeft).magnitude
+}
+
+matrix = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+diagonalDifference(arr: matrix) // returns 15
