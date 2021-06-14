8a78c8fae3cead23d7fdb95f5a70f21b04e5702f
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-14 20:13:51 +0500

    grading students problem

--- HackerRank-problems/arrays.swift
+++ HackerRank-problems/arrays.swift
@@ -128,3 +128,25 @@ extension Array where Element: Comparable {
 func birthdayCakeCandles(candles: [Int]) -> Int {
     return candles.count(candles.max()!)
 }
+
+// Grading Students (Easy)
+func nextMultipleOfFive(_ number: Int) -> Int {
+    if number % 5 == 0 {
+        return number
+    } else {
+        return nextMultipleOfFive(number+1)
+    }
+}
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    var result = [Int]()
+    
+    for grade in grades {
+        if grade >= 38 && (grade - nextMultipleOfFive(grade)).magnitude < 3 {
+            result.append(nextMultipleOfFive(grade))
+        } else {
+            result.append(grade)
+        }
+    }
+    return result
+}
