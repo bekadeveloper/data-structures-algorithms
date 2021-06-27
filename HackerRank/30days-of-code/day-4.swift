cf2ea2dfe16bcc8e776b3e07e7a6a58733a31bf1
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-27 08:29:31 +0500

    day 4: class and instance

---
+++ HackerRank/30days-of-code/day-4.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+class Person {
+    var age: Int = 0
+
+    init(initialAge: Int) {
+        if initialAge >= 0 {
+            age = initialAge
+        } else {
+            age = 0
+            print("Age is not valid, setting age to 0.")
+        }
+    }
+
+    func amIOld() {
+        if age < 13 {
+            print("You are young.")
+        } else if age >= 13 && age < 18 {
+            print("You are a teenager.")
+        } else {
+            print("You are old.")
+        }
+    }
+
+    func yearPasses() {
+        age += 1
+    }
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let age = Int(readLine()!)!
+    let p = Person(initialAge: age)
+
+    p.amIOld()
+
+    for i in 1...3 {
+        p.yearPasses()
+    }
+
+    p.amIOld()
+
+    print("")
+}
