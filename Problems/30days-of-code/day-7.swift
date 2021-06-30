273aa286ccbe11833d7d57d42ec2fa93aa451fb3
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-06-30 10:14:10 +0500

    day 7: arrays

---
+++ Problems/30days-of-code/day-7.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+var i = n - 1
+
+while i >= 0 {
+    print(arr[i], terminator: " ")
+    i -= 1
+}
