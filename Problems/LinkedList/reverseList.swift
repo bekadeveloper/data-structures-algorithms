486fad760b86754ef01b67ef09d84217490f2854
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-07-07 15:20:57 +0500

    more optimal algorithm to reverse linked list

--- Problems/LinkedList/reverseList.swift
+++ Problems/LinkedList/reverseList.swift
@@ -38,9 +38,27 @@ extension LinkedList {
     head = tempList.head
     tail = tempList.tail
   }
+
+
+  // 3 (more optimal algorithm for reversing a list)
+  mutating func optimalReverse() {
+        tail = head
+        var prev = head
+        var current = head?.next
+        prev?.next = nil
+        
+        while current != nil {
+            let next = current?.next
+            current?.next = prev
+            prev = current
+            current = next
+        }
+        
+        head = prev
+    }
 }
 
-
+// 1
 var list1 = LinkedList<String>()
 list1.append("Hello")
 list1.append("Linked")
@@ -51,10 +69,19 @@ list1.reverse()
 print("After reversing: \(list1)")
 
 
-
+// 2
 var list2 = LinkedList<Int>()
-(1...4).forEach { list.append($0) }
+(1...4).forEach { list2.append($0) }
 
 print("Before reversing: \(list2)")
 list2.easyReverse()
 print("After reversing: \(list2)")
+
+
+// 3
+var list3 = LinkedList<Int>()
+(1...4).forEach { list3.append($0) }
+
+print("Before reversing: \(list3)")
+list3.optimalReverse()
+print("After reversing: \(list3)")
