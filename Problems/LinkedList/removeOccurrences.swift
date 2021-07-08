d0a1d0fdbc2b4051c5e3e7cb5b00fd432992bda5
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-07-08 17:37:58 +0500

    easy way to remove all occurrences

--- Problems/LinkedList/removeOccurrences.swift
+++ Problems/LinkedList/removeOccurrences.swift
@@ -2,34 +2,18 @@ import Foundation
 
 extension LinkedList where Value: Equatable {
   
-  
+  // 1 (easy way)
   mutating func removeAll(_ value: Value) {
-    var stack = [Value]()
-    
-    var current = head
-    
-    while current != nil {
-        if current!.value != value {
-            stack.append(current!.value)
+    var tempList = LinkedList<Value>()
+
+    for element in self {
+        if element != value {
+            tempList.append(element)
         }
-        current = current!.next
     }
-    
-    guard !stack.isEmpty else {
-        head = nil
-        tail = nil
-        return
-    }
-    
-    head = Node(value: stack[0])
-    var current2 = head
-    
-    for index in 1..<stack.count {
-        current2!.next = Node(value: stack[index])
-        current2 = current2!.next
-    }
-    
-    tail = current2
+
+    head = tempList.head
+    tail = tempList.tail
   }
 }
 
