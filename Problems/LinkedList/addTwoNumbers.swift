5cd50107705e596dd76b7539e86cc2a6f9719548
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-07-10 18:22:07 +0500

    add two numbers

---
+++ Problems/LinkedList/addTwoNumbers.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+ 
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var node1 = l1
+        var node2 = l2
+        var anchor = 0
+        var resultNode = ListNode()
+        var current = resultNode
+        
+        while node1 != nil || node2 != nil || anchor == 1 {
+            let sum = (node1?.val ?? 0) + (node2?.val ?? 0) + anchor
+            
+            current.next = ListNode(sum % 10)
+            anchor = sum / 10
+            current = current.next!
+            
+            node1 = node1?.next
+            node2 = node2?.next
+        }
+        
+        return resultNode.next
+    }
+}
