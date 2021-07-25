5e236f9f44b89de07a351c2e5b6de03ef86bc913
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-07-25 11:10:55 +0500

    single number: brute force

--- Leetcode/arrays/singleNumber.swift
+++ Leetcode/arrays/singleNumber.swift
@@ -35,3 +35,24 @@ class Solution3 {
         return nums.reduce(0, ^)
     }
 }
+
+
+class BruteForceSolution {
+    func singleNumber(_ nums: [Int]) -> Int {
+        let sorted = nums.sorted()
+        var i = 0
+
+        while i < nums.count {
+            if i == nums.count - 1 {
+                return sorted.last!
+            } 
+
+            if sorted[i] == sorted[i+1] {
+                i += 2
+            } else {
+                return sorted[i]
+            }
+        }
+        return Int()
+    }
+}
