63495c20710e9d9656fe4193fec35e8b1aa81c5a
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-08-04 22:45:57 +0500

    intersection of two arrays: solution 3

--- Leetcode/arrays/intersection.swift
+++ Leetcode/arrays/intersection.swift
@@ -16,3 +16,17 @@ class Solution2 {
         Array(Set(nums1.filter { nums2.contains($0) }))
     }
 }
+
+class Solution3 {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var arr: [Int] = []
+        let smallest = nums1.count < nums2.count ? nums1 : nums2
+        
+        for i in Set(smallest) {
+            if smallest == nums1 && nums2.contains(i) || smallest == nums2 && nums1.contains(i) {
+                arr.append(i)
+            }
+        }
+        return arr
+    }
+}
